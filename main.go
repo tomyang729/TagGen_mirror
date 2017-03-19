6262fed2240c403ece0e2d511de9b1c714844ee6
qtoyoda <quinn.toyoda@uwaterlo.ca>
2017-03-19 02:59:47 -0700

    added px logic

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+
 	"github.com/clarifai/clarifai-go"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -10,11 +11,25 @@ import (
 
 func main() {
 
+	// NOTE: just for testing quinn's function
+	// fmt.Print("started getting hashtags\n")
+	// tags := make([]string, 3)
+	// tags[0] = "redbull"
+	// tags[1] = "starbucks"
+	// tags[2] = "coffee"
+	// // tags[3] = "tree"
+	// // tags[4] = "outside"
+	// retrievedTags := getPxTags(tags)
+	// for _, string := range retrievedTags {
+	// 	fmt.Print(string + "\n")
+	// }
+	// fmt.Print("finished getting hashtags")
+
 	app := gin.Default()
 	client := getClient()
 
 	server := &Server{
-		app: app,
+		app:    app,
 		client: client,
 	}
 
@@ -22,6 +37,7 @@ func main() {
 
 	// Run on 5050 port
 	app.Run(":5050")
+
 }
 
 func getClient() *clarifai.Client {
@@ -29,7 +45,7 @@ func getClient() *clarifai.Client {
 	//load secret keys file
 	err := godotenv.Load()
 	if err != nil {
-    	fmt.Print("Error loading .env file")
+		fmt.Print("Error loading .env file")
 	}
 
 	CLIENT_ID := os.Getenv("CLARIFAI_CLIENT_ID")
