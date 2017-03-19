03663b77b8c273e147a1342bfc60ec2a0e3967e7
Elad Michaeli <elad.michaeli.hootsuite.com>
2017-03-19 02:42:34 -0700

    getting response from clarifai api

--- server.go
+++ server.go
@@ -1,47 +1,128 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "html/template"
-    "net/http"
-    "github.com/clarifai/clarifai-go"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/clarifai/clarifai-go"
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"net/http"
+)
+
+const (
+	clarifaiApi = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
 )
 
 type Server struct {
-    app *gin.Engine
-    client *clarifai.Client
+	app    *gin.Engine
+	client *clarifai.Client
 }
 
 func (server *Server) Configure() {
-    app := server.app
+	app := server.app
 
-    // Load static resources & templates
-    customTemplate := template.Must(template.New("main").ParseGlob("resources/templates/base/*.tmpl"))
-    customTemplate.ParseGlob("resources/templates/*.tmpl")
-    app.SetHTMLTemplate(customTemplate)
-    app.Static("/static", "resources/static")
+	// Load static resources & templates
+	customTemplate := template.Must(template.New("main").ParseGlob("resources/templates/base/*.tmpl"))
+	customTemplate.ParseGlob("resources/templates/*.tmpl")
+	app.SetHTMLTemplate(customTemplate)
+	app.Static("/static", "resources/static")
 
-    // Homepage endpoint
-    app.GET("/", showHomePage)
+	// Homepage endpoint
+	app.GET("/", showHomePage)
 
-    app.GET("/fetch", fetchTags)
+	app.GET("/fetch", fetchTags)
 }
 
 
 func showHomePage(c *gin.Context) {
-    c.HTML(http.StatusOK, "index.tmpl", gin.H{
-        "title": "Home",
-    })
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title": "Home",
+	})
 }
+
 
 /*
    Get hashtags
-   Param: img
- */
+   Param: imgURL
+
+*/
 func fetchTags(c *gin.Context) {
-    // get img from request
-    // pass in to Clarifi
-    // use labels to hit Instagram/Twitter endpoint to get hashtags
-    // return array of hastags
+	params := c.Request.URL.Query()
+	image := params.Get("image")
+	if image == "" {
+		c.JSON(http.StatusBadRequest, "image parameter was not included")
+		return
+	}
+
+	req, err := getRequestBody(image)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	c.JSON(http.StatusOK, "Success!")
 }
 
+
+/*
+    Clarifai API request object body type:
+
+    {
+        "inputs": [
+           {
+                "data": {
+                    "image": {
+                        "url": "image-url"
+                    }
+                }
+            }
+        ]
+    }
+
+*/
+func getRequestBody(input string) (*http.Request, error) {
+	// for now, figure out how to make it one struct
+	type Url struct {
+		Url string `json:"url"`
+	}
+	type Image struct {
+		Image Url `json:"image"`
+	}
+	type Data struct {
+		Data Image `json:"data"`
+	}
+	type RequestBody struct {
+		Inputs []Data `json:"inputs"`
+	}
+
+	// Create request body struct
+	url := Url{input}
+	image := Image{url}
+	data := Data{image}
+	inputs := make([]Data, 0, 1)
+	inputs = append(inputs, data)
+	reqBody := RequestBody{inputs}
+
+	reqBodyBytes, err := json.Marshal(reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	body := bytes.NewReader(reqBodyBytes)
+	req, err := http.NewRequest("POST", clarifaiApi, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", `Bearer ThzzqYyVARtJdLbTQDwfRpa1FOk8w6`)
+	req.Header.Add("Content-Type", `application/json`)
+	return req, nil
+}
